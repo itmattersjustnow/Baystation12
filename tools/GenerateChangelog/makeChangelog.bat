1cffbbe8ff191b0dd9bad3f117a1b51eedf7663c
AshtonFox <31184078+AshtonFox@users.noreply.github.com>
2020-05-19 01:51:23 +0300

    Automatic Changelog Update
    Я извиняюсь за этот срач. Невнимателен.
    Алсо, гитигнор работает нереально криво (как ему вздумается, так и работает).

--- tools/GenerateChangelog/makeChangelog.bat
+++ tools/GenerateChangelog/makeChangelog.bat
@@ -1,6 +1,7 @@
 @echo off
 rem Cheridan asked for this. - N3X
 call py ss13_genchangelog.py ../../html/changelog_drex.html ../../html/changelogs
+pause
 git commit -m "Automatic Changelog Update" -a
 git push
 pause
